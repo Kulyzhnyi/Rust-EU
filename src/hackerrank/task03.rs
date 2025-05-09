bc76239ed7f261dae5cc783b71b08ef8e9835339
Kulyzhnyi <129198882+Kulyzhnyi@users.noreply.github.com>
2025-05-09 09:56:04 +0300

    Update task03.rs
    Задача з хакерранку. У завданні треба намалювати сходинки.
    Посилання на завдання: https://www.hackerrank.com/challenges/staircase/problem?isFullScreen=true

---
+++ src/hackerrank/task03.rs
@@ -0,0 +1,25 @@
+use std::io::{self, BufRead};
+
+/*
+ * Complete the 'staircase' function below.
+ *
+ * The function accepts INTEGER n as parameter.
+ */
+
+fn staircase(n: i32) {
+ for i in 1..=n {
+        let spaces = n - i;
+        let hashes = i;
+        let line = " ".repeat(spaces as usize) + &"#".repeat(hashes as usize);
+        println!("{}", line);
+    }
+}
+
+fn main() {
+    let stdin = io::stdin();
+    let mut stdin_iterator = stdin.lock().lines();
+
+    let n = stdin_iterator.next().unwrap().unwrap().trim().parse::<i32>().unwrap();
+
+    staircase(n);
+}
